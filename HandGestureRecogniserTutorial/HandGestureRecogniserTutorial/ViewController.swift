62051007c894541ffaa4822158a70c1f401dfd6d
Kynan Song <kynan.song@xdesign.com>
2020-07-06 11:15:01 +0100

    Created basic storyboard and model

---
+++ HandGestureRecogniserTutorial/HandGestureRecogniserTutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HandGestureRecogniserTutorial
+//
+//  Created by Kynan Song on 06/07/2020.
+//  Copyright © 2020 xDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var gestureView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HandGestureRecogniserTutorial/HandGestureRecogniserTutorial/Views/PlayerView.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerView.swift
+//  HandGestureRecogniserTutorial
+//
+//  Created by Kynan Song on 06/07/2020.
+//  Copyright © 2020 xDesign. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerView: UIView {
+    
+}
